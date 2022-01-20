92c330f1f12005bfd299594de616c778c286fa1a
LukeRekt <lucasrsl12@gmail.com>
2022-01-19 23:52:18 -0300

    updates basicos

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
   const nome = 'Maria';
   return (
     <div className="App">
-   
-    
+    <h1>Testando CSS</h1>
+    <Frase/>
+    <Frase/>
     <SayMyName nome="Lucas"/>
     <SayMyName nome={nome}/>
+
     <Pessoa
      nome="Lucas"
      idade="19"
-     profissao="Num Tem"
+     profissao="Dev L"
      foto="https://via.placeholder.com/150"/>
-    
+    <List/>
     </div>
 
   );
